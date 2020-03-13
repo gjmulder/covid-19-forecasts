5f643876b1c5309dc16228091bbf381b5b7c4121
Gary Mulder <gjmulder@gmail.com>
2020-03-13 17:17:27 +0200

    Adjusted graphs

--- plots-covid.R
+++ plots-covid.R
@@ -116,8 +116,8 @@ pop_death <-
   mutate_if(is.double, function(d) return(d/as.integer(.$population.1M))) %>%
   select(-Location, -population.1M) %>%
   gather(date.str, deaths, -country) %>%
-  filter(deaths > 0.001) %>%
   filter(! is.infinite(deaths)) %>%
+  filter(deaths > 0.1) %>%
   mutate(dt = mdy(date.str)) %>%
   group_by(country) %>%
   arrange(dt) %>%
@@ -133,134 +133,134 @@ gg <-
 
 print(gg)
 
-###########################################################################################################################
+# ###########################################################################################################################
+# #
+# # Ratio deaths per hospital beds
 #
-# Ratio deaths per hospital beds
-
-print("Beds - Unmatched countries (before renaming)")
-print(setdiff(
-  cov19_dead$country,
-  intersect(cov19_dead$country, hosp_beds$`Country Name`)
-))
-patt <- c(
-  "Brunei",
-  "Congo (Kinshasa)",
-  "Cruise Ship",
-  "Czechia",
-  "Egypt",
-  "French Guiana",
-  "Holy See",
-  "Iran",
-  "Korea, South",
-  "Martinique",
-  "Reunion",
-  "Russia",
-  "Slovakia",
-  "Taiwan*",
-  "US"
-)
-repl <- c(
-  "Brunei Darussalam",
-  "Congo, Dem. Rep.",
-  NA,
-  "Czech Republic",
-  "Egypt, Arab Rep.",
-  "Guyana",
-  NA,
-  "Iran, Islamic Rep.",
-  "Korea, Rep.",
-  NA,
-  NA,
-  "Russian Federation",
-  "Slovak Republic",
-  NA,
-  "United States"
-)
-cov19_dead$`Country Name` <- stri_replace_all_fixed(cov19_dead$country, patt, repl, vectorize_all = FALSE)
-print("Beds - Unmatched countries (after renaming)")
-print(setdiff(
-  cov19_dead$`Country Name`,
-  intersect(cov19_dead$`Country Name`, hosp_beds$`Country Name`)
-))
-
-###########################################################################################################################
-
-tot_world_pop <-
-  world_pop %>%
-  group_by(Location) %>%
-  summarise(population.1K = sum(PopMale + PopFemale))
-
-print("World Pop - Unmatched countries (before renaming)")
-print(setdiff(
-  cov19_dead$`Country Name`,
-  intersect(cov19_dead$`Country Name`, tot_world_pop$Location)
-))
-patt <- c(
-  "Bolivia (Plurinational State of)",
-  "Congo",
-  "Côte d'Ivoire",
-  "Czechia",
-  "Egypt",
-  "Iran (Islamic Republic of)",
-  "Republic of Korea",
-  "Republic of Moldova",
-  "Slovakia",
-  "United States of America",
-  "Viet Nam"
-)
-repl <- c(
-  "Bolivia",
-  "Congo, Dem. Rep.",
-  "Cote d'Ivoire",
-  "Czech Republic",
-  "Egypt, Arab Rep.",
-  "Iran, Islamic Rep.",
-  "Korea, Rep.",
-  "Moldova",
-  "Slovak Republic",
-  "United States",
-  "Vietnam"
-)
-tot_world_pop$`Country Name` <- stri_replace_all_fixed(tot_world_pop$Location, patt, repl, vectorize_all = FALSE)
-print("World Population - Unmatched countries (after renaming)")
-print(setdiff(
-  cov19_dead$`Country Name`,
-  intersect(cov19_dead$`Country Name`, tot_world_pop$`Country Name`)
-))
-
-###########################################################################################################################
-
-hosp_beds_max <-
-  hosp_beds %>%
-  select(-`Country Code`, -`Indicator Name`, -`Indicator Code`) %>%
-  gather(year, beds.year, -`Country Name`) %>%
-  group_by(`Country Name`) %>%
-  summarise(beds.1K = max(beds.year, na.rm = TRUE)) %>%
-  filter(! is.infinite(beds.1K))
-
-beds_tot <-
-  inner_join(hosp_beds_max, tot_world_pop) %>%
-  mutate(beds.1M = as.character(beds.1K * population.1K / 1E6)) %>%
-  select(`Country Name`, beds.1M)
-
-beds_death <-
-  inner_join(beds_tot, cov19_dead) %>%
-  mutate_if(is.double, function(d) return(d/as.numeric(.$beds.1M))) %>%
-  select(-Location, -`Country Name`, -beds.1M) %>%
-  gather(date.str, deaths, -country) %>%
-  filter(deaths > 1.0) %>%
-  filter(! is.infinite(deaths)) %>%
-  mutate(dt = mdy(date.str)) %>%
-  group_by(country) %>%
-  arrange(dt) %>%
-  mutate(day.number = dplyr::row_number())
-
-gg <-
-  ggplot(beds_death, aes(x = day.number, y = deaths, colour = country)) +
-  geom_line() +
-  scale_y_log10() +
-  xlab("Day number") +
-  ylab("Deaths per 1M hospital beds") +
-  geom_dl(aes(label = country), method = list(dl.combine("last.points"), rot=-30, cex=0.7))
-
-print(gg)
+# print("Beds - Unmatched countries (before renaming)")
+# print(setdiff(
+#   cov19_dead$country,
+#   intersect(cov19_dead$country, hosp_beds$`Country Name`)
+# ))
+# patt <- c(
+#   "Brunei",
+#   "Congo (Kinshasa)",
+#   "Cruise Ship",
+#   "Czechia",
+#   "Egypt",
+#   "French Guiana",
+#   "Holy See",
+#   "Iran",
+#   "Korea, South",
+#   "Martinique",
+#   "Reunion",
+#   "Russia",
+#   "Slovakia",
+#   "Taiwan*",
+#   "US"
+# )
+# repl <- c(
+#   "Brunei Darussalam",
+#   "Congo, Dem. Rep.",
+#   NA,
+#   "Czech Republic",
+#   "Egypt, Arab Rep.",
+#   "Guyana",
+#   NA,
+#   "Iran, Islamic Rep.",
+#   "Korea, Rep.",
+#   NA,
+#   NA,
+#   "Russian Federation",
+#   "Slovak Republic",
+#   NA,
+#   "United States"
+# )
+# cov19_dead$`Country Name` <- stri_replace_all_fixed(cov19_dead$country, patt, repl, vectorize_all = FALSE)
+# print("Beds - Unmatched countries (after renaming)")
+# print(setdiff(
+#   cov19_dead$`Country Name`,
+#   intersect(cov19_dead$`Country Name`, hosp_beds$`Country Name`)
+# ))
+#
+# ###########################################################################################################################
+#
+# tot_world_pop <-
+#   world_pop %>%
+#   group_by(Location) %>%
+#   summarise(population.1K = sum(PopMale + PopFemale))
+#
+# print("World Pop - Unmatched countries (before renaming)")
+# print(setdiff(
+#   cov19_dead$`Country Name`,
+#   intersect(cov19_dead$`Country Name`, tot_world_pop$Location)
+# ))
+# patt <- c(
+#   "Bolivia (Plurinational State of)",
+#   "Congo",
+#   "Côte d'Ivoire",
+#   "Czechia",
+#   "Egypt",
+#   "Iran (Islamic Republic of)",
+#   "Republic of Korea",
+#   "Republic of Moldova",
+#   "Slovakia",
+#   "United States of America",
+#   "Viet Nam"
+# )
+# repl <- c(
+#   "Bolivia",
+#   "Congo, Dem. Rep.",
+#   "Cote d'Ivoire",
+#   "Czech Republic",
+#   "Egypt, Arab Rep.",
+#   "Iran, Islamic Rep.",
+#   "Korea, Rep.",
+#   "Moldova",
+#   "Slovak Republic",
+#   "United States",
+#   "Vietnam"
+# )
+# tot_world_pop$`Country Name` <- stri_replace_all_fixed(tot_world_pop$Location, patt, repl, vectorize_all = FALSE)
+# print("World Population - Unmatched countries (after renaming)")
+# print(setdiff(
+#   cov19_dead$`Country Name`,
+#   intersect(cov19_dead$`Country Name`, tot_world_pop$`Country Name`)
+# ))
+#
+# ###########################################################################################################################
+#
+# hosp_beds_max <-
+#   hosp_beds %>%
+#   select(-`Country Code`, -`Indicator Name`, -`Indicator Code`) %>%
+#   gather(year, beds.year, -`Country Name`) %>%
+#   group_by(`Country Name`) %>%
+#   summarise(beds.1K = max(beds.year, na.rm = TRUE)) %>%
+#   filter(! is.infinite(beds.1K))
+#
+# beds_tot <-
+#   inner_join(hosp_beds_max, tot_world_pop) %>%
+#   mutate(beds.1M = as.character(beds.1K * population.1K / 1E6)) %>%
+#   select(`Country Name`, beds.1M)
+#
+# beds_death <-
+#   inner_join(beds_tot, cov19_dead) %>%
+#   mutate_if(is.double, function(d) return(d/as.numeric(.$beds.1M))) %>%
+#   select(-Location, -`Country Name`, -beds.1M) %>%
+#   gather(date.str, deaths, -country) %>%
+#   filter(deaths > 1.0) %>%
+#   filter(! is.infinite(deaths)) %>%
+#   mutate(dt = mdy(date.str)) %>%
+#   group_by(country) %>%
+#   arrange(dt) %>%
+#   mutate(day.number = dplyr::row_number())
+#
+# gg <-
+#   ggplot(beds_death, aes(x = day.number, y = deaths, colour = country)) +
+#   geom_line() +
+#   scale_y_log10() +
+#   xlab("Day number") +
+#   ylab("Deaths per 1M hospital beds") +
+#   geom_dl(aes(label = country), method = list(dl.combine("last.points"), rot=-30, cex=0.7))
+#
+# print(gg)
